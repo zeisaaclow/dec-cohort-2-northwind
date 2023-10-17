ecc8be58e450dd8f506a1c4fcf1cd1bbcfbf4f1d
Isaac Low <zy.isaaclow@gmail.com>
2023-10-17 20:41:58 +1100

    documentation: explain purpose of test

--- northwind_elt/tests/generic/is_before_or_equal_to.sql
+++ northwind_elt/tests/generic/is_before_or_equal_to.sql
@@ -1,4 +1,5 @@
 {% test is_before_or_equal_to(model, column_name, before_date) %}
+    -- This test checks that the ship date comes after or equal to the order date
     WITH shipped AS (
         select * from {{ model }} where shipped_date is not null
     )
